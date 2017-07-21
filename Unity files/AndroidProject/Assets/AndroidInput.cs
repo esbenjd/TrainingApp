9c8bf394872ba31f89833a729a6c403cdd7d9207
Tue Z <Tue Z>
2017-07-22 00:31:54 +0200

    first script

---
+++ Unity files/AndroidProject/Assets/AndroidInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidInput : MonoBehaviour {
+
+    public float minTouchDistance;
+    public Material pushed;
+    public Material normal; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Touch[] t = Input.touches; 
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (Vector2.Distance(t[i].position, transform.position) < minTouchDistance)
+            {
+                Pushed(); 
+            }
+        }
+	}
+
+    void Pushed()
+    {
+        if (pushed != null)
+        {
+            GetComponent<Renderer>().material = pushed;
+        }
+    }
+}
